bc4b89d98a57e3a3d14b894d2d9ca1667d73beb7
Nazer Batalla <nazer.batalla@whitecobalt.com>
2021-05-12 11:56:34 +0800

    update

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.get("/" ,(req,res, next) => {
     res.json({message:"hello word"});
 });
 
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{ console.log(`Server running on port ${port}`)});
